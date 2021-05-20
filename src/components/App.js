01d4fb58746c26680408ead86036ae59caf71e14
PhilDL <philippe.lattention@hotmail.fr>
2021-05-20 15:25:49 +0400

    [IMP] Name conventions, Single file component convention

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import { Component, useState, tags } from "@odoo/owl";
+import { Navbar } from "./Navbar";
+
+const APP_TEMPLATE = tags.xml/*xml*/ `
+<main>
+  <Navbar/>
+  <div t-name="App" class="container" t-on-click="update">
+    Hello <t t-esc="state.text"/>
+  </div>
+</main>
+`;
 
 export class App extends Component {
+  static components = { Navbar };
+  static template = APP_TEMPLATE;
   state = useState({ text: "Owl" });
   update() {
     this.state.text = this.state.text === "Owl" ? "World" : "Owl";
   }
 }
-
-App.template = tags.xml/*xml*/ `
-<div t-name="App" class="bg-white shadow m-8 p-2 rounded cursor-pointer" t-on-click="update">
-  Hello <t t-esc="state.text"/>
-</div>
-`;
