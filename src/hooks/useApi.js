1af03f858a0cf24cf94873cb3e6869e2a1e555fa
Philippe L'ATTENTION <PhilDL@users.noreply.github.com>
2021-11-09 07:54:52 +0400

    [IMP] Use new API URL (#31)

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -5,7 +5,7 @@ import axios from "redaxios";
 export class ApiService {
   constructor(token) {
     let config = {
-      baseURL: "https://conduit.productionready.io/api",
+      baseURL: "https://api.realworld.io/api",
       timeout: 1000,
     };
     if (token) {
